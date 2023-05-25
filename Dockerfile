7501cf276e89de27f91619eb6a7c61ac08a46390
Jonatan Lopez <jonatan.lopez@exfo.com>
2023-05-25 20:05:24 +0200

    Fix connection to mongo database

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.10
+FROM python:3.8.16-slim-buster
 
 WORKDIR /usr/src/app
 
--- app/infrastructure/mongoDB/mongoDB_utils.py
+++ app/infrastructure/mongoDB/mongoDB_utils.py
@@ -2,16 +2,23 @@ import logging
 from pymongo import MongoClient
 from .MongoDBSession import db
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 def connect_to_mongo(uri, db_name):
-    logger.info("Connecting to database...\nuri: " + uri + "\ndb_name: " + db_name)
-    db.client = MongoClient(uri)
-    db.db = db.client[db_name]
-
-    logger.info("Database connected！")
+    try:
+        logger.info("Connecting to database...\nuri: %s\ndb_name: %s", uri, db_name)
+        db.client = MongoClient(uri)
+        db.db = db.client[db_name]
+        logger.info("Database connected!")
+    except Exception as e:
+        logger.error("Error connecting to database: %s", str(e))
+        raise
 
 def close_mongo_connection():
-    logger.info("Closing database connection...")
-    db.client.close()
-    logger.info("Database closed！")
+    try:
+        logger.info("Closing database connection...")
+        db.client.close()
+        logger.info("Database closed!")
+    except Exception as e:
+        logger.error("Error closing database connection: %s", str(e))
+        raise
